a12a9229e944f54c363a3669dc3f2add8d3c4fbc
SBrazzCode <Shaun.Brazzoni@edu.sait.ca>
2021-01-17 21:57:57 -0700

    Reorganize Directories and Add colorfulServer Example

--- colorfulServer/colorfulServer.js
+++ colorfulServer/colorfulServer.js
@@ -1,21 +1,24 @@
 const http = require('http');
+const chalk = require('chalk');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer( (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
-    process.kill(process.pid, 'SIGTERM');
+
+    process.kill(pid, 'SIGTERM');
+
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(chalk.green(`Server running at http://${hostname}:${port}/`));
 });
 
 process.on('SIGTERM', () => {
     server.close(() => {
-        console.log('Process terminated')
+        console.log('Process terminated');
     })
 });
---
+++ simpleServer/simpleServer.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const chalk = require('chalk');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer( (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+
+    process.kill(pid, 'SIGTERM');
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(chalk.green(`Server running at http://${hostname}:${port}/`));
+});
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated');
+    })
+});
